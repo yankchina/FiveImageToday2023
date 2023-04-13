8f4710afe30e1a016cddef9954c63635c356ba9d
yankchina <yankchina@gmail.com>
2023-04-13 13:28:43 +0800

    Add ID String

--- FiveImageToday/Views/Home.swift
+++ FiveImageToday/Views/Home.swift
@@ -10,20 +10,36 @@ import SwiftUI
 struct Home: View{
     @State private var currentCard: String = ""
     @State private var listOfCards: [ImageCard] = []
+    private let _height:CGFloat = 600.0
+    private let _width: CGFloat = 340.0
+    private let _cornerRadius: CGFloat = 25.0
+    private let _imageWidth: CGFloat = 600.0
+    private let _imageHeight: CGFloat = 340.0
     var body: some View{
         GeometryReader{
             let size = $0.size
             TabView(selection: $currentCard, content: {
                 ForEach(listOfCards){ Card in
-                    RoundedRectangle(cornerRadius: 25, style: .continuous)
-                        .fill(Card.color.gradient)
-                        .frame(width: 340, height: size.height)
-                        .tag(Card.id.uuidString)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: self._cornerRadius, style: .continuous)
+                            .fill(Card.color.gradient)
+                            .frame(width: self._width, height: size.height)
+                            .tag(Card.id.uuidString)
+                        if( Card.image_url != "" ){
+                            AsyncImage(url: URL(string:Card.image_url))
+                                .frame(width: self._imageWidth, height: self._imageHeight)
+                        }
+                        else
+                        {
+                            Text("Card ID \(Card.id.uuidString)")
+                                .frame(width: self._width - 200, height: size.height)
+                        }
+                    }
                 }
             })
             .tabViewStyle(.page(indexDisplayMode: .never))
         }
-        .frame(height:600)
+        .frame(height: self._height)
         .onAppear{
             /// loading listOfCards
             for color in [Color.red, Color.blue, Color.yellow,Color.orange,Color.brown]{
