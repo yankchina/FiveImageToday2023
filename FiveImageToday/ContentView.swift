9acf5d6a90865e420747dabe2dfe8af6e49547ad
yankchina <yankchina@gmail.com>
2023-04-13 13:09:01 +0800

    Five Rounded Rectangle

--- FiveImageToday/ContentView.swift
+++ FiveImageToday/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Home()
+        NavigationStack{
+            Home()
+                .navigationTitle("Five Image Today")
+        }
     }
 }
 
--- FiveImageToday/Views/Home.swift
+++ FiveImageToday/Views/Home.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct Home: View{
+    @State private var currentCard: String = ""
+    @State private var listOfCards: [ImageCard] = []
     var body: some View{
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{
+            let size = $0.size
+            TabView(selection: $currentCard, content: {
+                ForEach(listOfCards){ Card in
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Card.color.gradient)
+                        .frame(width: 340, height: size.height)
+                        .tag(Card.id.uuidString)
+                }
+            })
+            .tabViewStyle(.page(indexDisplayMode: .never))
         }
-        .padding()
+        .frame(height:600)
+        .onAppear{
+            /// loading listOfCards
+            for color in [Color.red, Color.blue, Color.yellow,Color.orange,Color.brown]{
+                listOfCards.append(.init(color:color))
+            }
+        }
     }
 }
