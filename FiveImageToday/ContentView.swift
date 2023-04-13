ec3804576e9f7fec0b7d3b57f506d00cb69998da
yankchina <yankchina@gmail.com>
2023-04-13 12:49:32 +0800

    Init App

---
+++ FiveImageToday/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FiveImageToday
+//
+//  Created by apple on 2023/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FiveImageToday/FiveImageTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveImageTodayApp.swift
+//  FiveImageToday
+//
+//  Created by apple on 2023/4/13.
+//
+
+import SwiftUI
+
+@main
+struct FiveImageTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
